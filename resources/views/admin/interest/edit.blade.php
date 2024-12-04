fc796a6ab4f8302e5d8e66634cddcb8fe67ec6a4
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-12-04 12:44:17 +0700

    Bidang minat

--- resources/views/admin/interest/edit.blade.php
+++ resources/views/admin/interest/edit.blade.php
@@ -9,52 +9,53 @@
         </div>
         <div class="modal-body">
             <div class="alert alert-danger">
-                <h5><i class="bi bi-x-circle-fill"></i> Kesalahan!!!</h5> <!-- Updated icon -->
-                Data yang anda cari tidak ditemukan
+                <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
+                Data yang anda cari tidak ditemukan.
             </div>
             <a href="{{ url('/interest') }}" class="btn btn-warning">Kembali</a>
         </div>
     </div>
 </div>
 @else
-<form action="{{ url('/interest/' . $interest['interest_id'] . '/delete_ajax') }}" method="POST" id="form-delete">
+<form action="{{ url('/interest/' . $interest['interest_id'] . '/update') }}" method="POST" id="form-edit">
     @csrf
-    @method('DELETE')
+    @method('PUT')
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Hapus Data Bidang Minat</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Edit Data Bidang Minat</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="alert alert-warning">
-                    <h5><i class="bi bi-exclamation-triangle-fill"></i> Konfirmasi !!!</h5> <!-- Updated icon -->
-                    Apakah Anda ingin menghapus data seperti dibawah ini?
+                <div class="form-group">
+                    <label for="interest_code">Kode Bidang Minat</label>
+                    <input value="{{ old('interest_code', $interest['interest_code']) }}" type="text" name="interest_code" id="interest_code" class="form-control" required>
+                    <small id="error-interest_code" class="error-text form-text text-danger"></small>
                 </div>
-                <table class="table table-sm table-bordered table-striped">
-                    <tr>
-                        <th class="text-right col-3">Kode Bidang Minat:</th>
-                        <td class="col-9">{{ $interest['interest_code'] }}</td>
-                    </tr>
-                    <tr>
-                        <th class="text-right col-3">Nama Bidang Minat:</th>
-                        <td class="col-9">{{ $interest['interest_name'] }}</td>
-                    </tr>
-                </table>
+                <div class="form-group">
+                    <label for="interest_name">Nama Bidang Minat</label>
+                    <input value="{{ old('interest_name', $interest['interest_name']) }}" type="text" name="interest_name" id="interest_name" class="form-control" required>
+                    <small id="error-interest_name" class="error-text form-text text-danger"></small>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
-                <button type="submit" class="btn btn-primary">Ya, Hapus</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
             </div>
         </div>
     </div>
 </form>
+
 <script>
     $(document).ready(function() {
-            $("#form-delete").validate({
-                rules: {},
+            $("#form-edit").validate({
+                rules: {
+                    interest_id: { required: true, number: true },
+                    interest_code: { required: true, minlength: 3, maxlength: 20 },
+                    interest_name: { required: true, minlength: 3, maxlength: 100 },
+                },
                 submitHandler: function(form) {
                     $.ajax({
                         url: form.action,
@@ -72,7 +73,7 @@
                             } else {
                                 $('.error-text').text('');
                                 $.each(response.msgField, function(prefix, val) {
-                                    $('#error-'+prefix).text(val[0]);
+                                    $('#error-' + prefix).text(val[0]);
                                 });
                                 Swal.fire({
                                     icon: 'error',
@@ -85,18 +86,17 @@
                     return false;
                 },
                 errorElement: 'span',
-                errorPlacement: function (error, element) {
-                    error.addClass("invalid-feedback");
-                    elemtnt.closest('.form-group').append(error);
+                errorPlacement: function(error, element) {
+                    error.addClass('invalid-feedback');
+                    element.closest('.form-group').append(error);
                 },
-                highlight: function (element, errorClass, validClass) {
+                highlight: function(element, errorClass, validClass) {
                     $(element).addClass('is-invalid');
                 },
-                unhighlight: function (element, errorClass, validClass) {
+                unhighlight: function(element, errorClass, validClass) {
                     $(element).removeClass('is-invalid');
                 }
             });
         });
-    </script>
 </script>
 @endempty
